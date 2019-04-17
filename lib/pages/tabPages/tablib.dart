3719c66772f93156e8af421d223e3f29c44ae1b6
Chris <patte539@umn.edu>
2019-04-17 13:20:38 -0500

    Revert to material style

--- lib/pages/tabPages/tablib.dart
+++ lib/pages/tabPages/tablib.dart
@@ -1,12 +1,9 @@
 library tab_lib;
 
-
-
-import 'dart:io';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:savour_deals_flutter/containers/dealCardWidget.dart';
 import 'package:savour_deals_flutter/containers/vendorCardWidget.dart';
@@ -19,6 +16,8 @@ import 'package:geolocator/geolocator.dart';
 import 'package:simple_permissions/simple_permissions.dart';
 import 'package:flutter_geofire/flutter_geofire.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+
 
 
 part "dealsTabPage.dart";
